0cae6827f73ef390006a42c44937bb9f48791c47
elizabethtarantola <liztarantola@gmail.com>
2021-04-19 18:24:48 -0500

    first 3 parts of exercise 1

---
+++ src/exercises/AreaOfRectangle.java
@@ -0,0 +1,24 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class AreaOfRectangle {
+    public static void main(String[] args) {
+        int length;
+        int width;
+        int area;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the length of the rectangle:");
+        length = input.nextInt();
+
+        Scanner moreInput = new Scanner(System.in);
+        System.out.println("Enter the width of the rectangle:");
+        width = moreInput.nextInt();
+
+
+        area = length * width;
+        System.out.println("The area of the rectangle is: " + area);
+
+    }
+}
---
+++ src/exercises/MilesPerGallon.java
@@ -0,0 +1,22 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class MilesPerGallon {
+    public static void main(String[] args) {
+        float miles;
+        float gasUsed;
+        float milesPerGallon;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter miles driven:");
+        miles = input.nextFloat();
+
+        Scanner moreInput = new Scanner(System.in);
+        System.out.println("Enter gallons of gas used:");
+        gasUsed = moreInput.nextFloat();
+
+        milesPerGallon = miles/gasUsed;
+        System.out.println("Your care gets: " + milesPerGallon + "per gallon");
+    }
+}
