e5b9a90817bcab192acc882736de7571a29521a9
elizabethtarantola <liztarantola@gmail.com>
2021-04-25 21:23:33 -0500

    Exercises Classes and Objects in progress

---
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -0,0 +1,9 @@
+package org.launchcode.java.demos.lsn3classes1;
+
+import java.util.HashMap;
+
+public class Course {
+    protected String teacher;
+    protected double grades;
+    protected HashMap<String, Integer> students = new HashMap<>();
+}
